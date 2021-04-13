3f7e43c52e49f331474c981394f0c15217862dd9
paco <paco@core>
2021-04-12 19:09:13 -0500

    todo funciona como se esperaba, el algoritmo es capaz de encontrar todas las posibles combinaciones, incluso las que son iguales pero estan en diferente orden, falta ordenar esa data y limpiarla para no tener combinaciones repetidas

--- main.dart
+++ main.dart
@@ -4,25 +4,25 @@ import 'limpiar.dart';
 
 main(List<String> args) {
   var tamano = 4;
-  var columnas = [1, 2, 3];
-  var filas = [1, 2, 3];
+  var columnas = [1, 2, 3, 4];
+  var filas = [1, 2, 3,4];
   var total_casillas = [
     [1, 1],
     [1, 2],
     [1, 3],
-    // [1, 4],
+    [1, 4],
     [2, 1],
     [2, 2],
     [2, 3],
-    // [2, 4],
+    [2, 4],
     [3, 1],
     [3, 2],
     [3, 3],
-    // [3, 4],
-    // [4, 1],
-    // [4, 2],
-    // [4, 3],
-    // [4, 4],
+    [3, 4],
+    [4, 1],
+    [4, 2],
+    [4, 3],
+    [4, 4],
   ];
   var data = SingletonData();
   total_casillas.forEach((element) => data.casillas.add(element.toString()));
